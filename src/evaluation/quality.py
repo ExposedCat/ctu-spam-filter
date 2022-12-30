ac33cf7ab7b30e7695b2626f92404acff5d7aad5
pepfof <artemiodro@gmail.com>
2022-12-30 16:13:24 +0100

    This time, EXTREMELY important update

--- src/evaluation/quality.py
+++ src/evaluation/quality.py
@@ -18,3 +18,15 @@ class QualityComputor:
         confmat = BinaryConfusionMatrix(pos_tag='OK', neg_tag='SPAM')
         confmat.compute_from_dicts(truth, prediction)
         return QualityComputor.quality_score(**confmat.as_dict())
+
+    def compute_on_memory_file(corpus_dir: str, prediction_file):
+        truth = EvaluationUtils.read_classification_from_file(
+            f'{corpus_dir}/!truth.txt'
+        )
+        prediction_file.seek(0)
+        prediction = EvaluationUtils.read_classification_from_memory_file(
+            prediction_file
+        )
+        confmat = BinaryConfusionMatrix(pos_tag='OK', neg_tag='SPAM')
+        confmat.compute_from_dicts(truth, prediction)
+        return QualityComputor.quality_score(**confmat.as_dict())
--- src/filter.py
+++ src/filter.py
@@ -3,47 +3,75 @@ from helpers.writer import Writer
 from services.word_evaluator import Wordset, WeightedWordDict
 from services.word_matcher import WordMatcher
 from evaluation.quality import QualityComputor
+import shutil
+import os
 
 
-class SpamFilter:
-    def __init__(
-        self,
-        directory: str = abspath("../test/first"),
-        ham_tag: str = "OK",
-        spam_tag: str = "SPAM",
-    ):
-        self.ham_counter = Wordset(
-            f'{directory}/!truth.txt', tag=ham_tag
-        ).count()
+class MyFilter:
+    def __init__(self, spam_tag="SPAM", ok_tag="OK"):
+        self.spam_tag = spam_tag
+        self.ok_tag = ok_tag
+        self.trained = 0
+        pass
+
+    def train(self, set_dir, train_range=(-100, 20), desired_freq_cutoff=1, training_freq_cutoff=100, logs=False):
+
+        self.trained = 1
+
+        self.ok_counter = Wordset(
+            f'{set_dir}/!truth.txt', tag=self.ok_tag).to_counter()
         self.spam_counter = Wordset(
-            f'{directory}/!truth.txt', tag=spam_tag
-        ).count()
-        self.weighted_words = WeightedWordDict(
-            self.ham_counter, self.spam_counter, 100
-        )
+            f'{set_dir}/!truth.txt', tag=self.spam_tag).to_counter()
 
-    def get_max_value(
-        self,
-        first_dir: str = abspath("../test/first"),
-        second_dir: str = abspath("../test/second"),
-        logs: bool = True,
-    ) -> None:
-        writer = Writer("Max Value", logs)
+        self.weighted_words = WeightedWordDict(
+            self.ok_counter, self.spam_counter, training_freq_cutoff)
+
+        writer = Writer("Training", logs)  # нахуя а главное зачем
+
         maxval = (0, 0)
-        for i in range(-100, 0):
-            WordMatcher.debug_check_corpus(
-                f'{first_dir}/!truth.txt', self.weighted_words, i / 100
+        t1, t2 = train_range
+        for i in range(t1, t2):
+            prediction_file = WordMatcher.try_on_corpus(
+                f'{set_dir}/!truth.txt', self.weighted_words, i / 100
             )
-            quality = QualityComputor.compute(first_dir)
+            prediction_file.seek(0)
+            quality = QualityComputor.compute_on_memory_file(
+                set_dir, prediction_file)
             if maxval[0] < quality:
                 maxval = quality, i
             writer.print((round(quality, 2), i / 100))
+            prediction_file.close()
         writer.print(f"Max value: {round(maxval[0],2)} {maxval[1]/100}")
 
+        self.max_val = maxval[1]/100
+
         self.weighted_words = WeightedWordDict(
-            self.ham_counter, self.spam_counter, 1
+            self.ok_counter, self.spam_counter, desired_freq_cutoff)
+
+    def manual_train(self, set_dir, val, logs=False):
+        self.ok_counter = Wordset(
+            f'{set_dir}/!truth.txt', tag=self.ok_tag).to_counter()
+        self.spam_counter = Wordset(
+            f'{set_dir}/!truth.txt', tag=self.spam_tag).to_counter()
+
+        self.weighted_words = WeightedWordDict(
+            self.ok_counter, self.spam_counter, 100)
+
+        self.max_val = val
+
+    def set_max_val(self, val):
+        self.max_val = val
+
+    def test(self, set_dir, logs: bool = False):
+        file = open(f'{set_dir}/!prediction.txt', "w", encoding="utf-8")
+        if (not self.trained):
+            for filen in [
+                filename
+                for filename in os.listdir(set_dir)
+                if filename[0] != '!']:
+                file.write(f"{filen} OK\n")
+            file.close()
+            return
+        WordMatcher.test_on_corpus(
+            f'{set_dir}', self.weighted_words, self.max_val,
         )
-        WordMatcher.debug_check_corpus(
-            f'{second_dir}/!truth.txt', self.weighted_words, maxval[1] / 100
-        )
-        writer.print(QualityComputor.compute(second_dir))
